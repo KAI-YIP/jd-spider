d9a06359a2e4538a931ebe0763c76c7cfb07f201
叶开 <tingxueye@gmail.com>
2015-03-09 19:41:04 +0800

    v1.0

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 #coding=utf-8
 f=open('./test/手机_手机通讯_手机_id_price.txt','r',encoding='utf-8')
+ID_list=[]
 for line in f.readlines():
-	print (line)
-	break
+	line_clean=line.strip().split('///')
+	ID_list.append(line_clean[1])
+print (ID_list)
+
+
